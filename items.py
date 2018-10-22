3c08787c85d89b1f6a3929694bbfba6a684cedf7
shadowhelo <shadowhelo@hotmail.co.uk>
2018-10-22 17:39:51 +0100

    First Backup

---
+++ items.py
@@ -0,0 +1,32 @@
+item_survival_kit = {
+    "id": "survival kit",
+
+    "name": "kirill's survival kit",
+
+    "description":
+    "A dusty green box labelled 'kirills DO NOT TOUCH!'."
+}
+
+item_love_potion = {
+    "id": "love potion",
+
+    "name": "Amortentia",
+
+    "description": "A potent love potion just one whiff and the victim falls helplessly in love."
+}
+
+item_sword = {
+    "id": "sword",
+
+    "name": "The Atlantean Sword",
+
+    "description": "The Atlantean Sword is a bastard sword that once belonged to an ancient king who, along with this sword and several servants, was buried in a small tomb."
+}
+
+item_arkenstone = {
+    "id": "arkenstone",
+
+    "name": "arkenstone",
+
+    "description": "A wondrous large white gem, the royal jewel of the Dwarf-kingdom of Erebor."
+}
---
+++ player.py
@@ -0,0 +1,9 @@
+from items import *
+from map import rooms
+
+inventory = []
+
+player_name = ""
+
+# Start game at the reception
+current_room = rooms["Castle_Grounds"]
